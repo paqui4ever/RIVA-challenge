ab8d800cdcb52f07c453032ddabefb1d2054ef9f
Agustin Picard <agustin.picard@gmail.com>
2026-02-07 16:05:46 +0100

    rcnn_v2: fix anchor shape mismatch

--- models/sam3_rcnn_v2.py
+++ models/sam3_rcnn_v2.py
@@ -151,7 +151,7 @@ def build_sam3_fasterrcnn(
     featmap_names = ["0", "1", "2", "3"]
 
     # Adjusted for 1008x1008 input size (Original 100px objects become ~98px)
-    anchor_sizes = ((98.4375,), (98.4375,), (98.4375,), (98.4375,), (98.4375,)) # Takes into account resize of the image
+    anchor_sizes = ((98.4375,), (98.4375,), (98.4375,), (98.4375,)) # Takes into account resize of the image
     aspect_ratios = ((1.0,),) * len(anchor_sizes) 
     
     anchor_generator = AnchorGenerator(
